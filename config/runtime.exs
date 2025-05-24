a8f6db4b45f72e00b4aeb00274a3f29b2b290bca
Lyu Ji <hkfhfkcoc@protonmail.com>
2025-05-24 03:22:26 -0700

    so stupid...

--- config/runtime.exs
+++ config/runtime.exs
@@ -10,7 +10,7 @@ if config_env() == :prod do
   maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
 
   config :teto_bot, TetoBot.Repo,
-    database: database_url,
+    url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6
 end
