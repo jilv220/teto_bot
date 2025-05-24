dbf8b23dea6b5612aa13971255199bfd57d8686b
Lyu Ji <hkfhfkcoc@protonmail.com>
2025-05-24 02:36:07 -0700

    a

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,14 +1,16 @@
 import Config
 
 if config_env() == :prod do
-  database_path =
+  database_url =
     System.get_env("DATABASE_URL") ||
       raise """
       environment variable DATABASE_URL is missing.
       """
 
-  config :name, Name.Repo,
-    database: database_path,
+  maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
+
+  config :teto_bot, TetoBot.Repo,
+    database: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    ssl: true
+    socket_options: maybe_ipv6
 end
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TetoBot.MixProject do
     [
       app: :teto_bot,
       version: "0.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
