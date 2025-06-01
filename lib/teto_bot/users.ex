41c2c679ca6ff0aa4505bc6f8c7736b230cc8710
Lyu Ji <hkfhfkcoc@protonmail.com>
2025-05-31 20:35:41 -0700

    fix user_guilds

--- lib/teto_bot/users.ex
+++ lib/teto_bot/users.ex
@@ -1,4 +1,7 @@
 defmodule TetoBot.Users do
+  alias Nostrum.Snowflake
+  alias Ecto.Multi
+
   alias TetoBot.Repo
   alias TetoBot.Users.User
 
@@ -34,24 +37,43 @@ defmodule TetoBot.Users do
       iex> TetoBot.Users.update_last_interaction(invalid_user_id)
       {:error, %Ecto.Changeset{}}
   """
-  @spec update_last_interaction(integer()) :: {:ok, User} | {:error, Ecto.Changeset.t()}
-  def update_last_interaction(user_id) do
+  @spec update_last_interaction(Snowflake.t(), Snowflake.t()) ::
+          {:ok, User} | {:error, Ecto.Changeset.t()}
+  def update_last_interaction(guild_id, user_id) do
     now = DateTime.utc_now()
 
-    %User{user_id: user_id}
-    |> User.changeset(%{last_interaction: now})
-    |> Repo.insert(
+    Multi.new()
+    |> Multi.insert(
+      :user,
+      User.changeset(%User{user_id: user_id}, %{last_interaction: now}),
       on_conflict: [set: [last_interaction: now, updated_at: now]],
       conflict_target: :user_id
     )
+    |> Multi.run(:user_guild, fn _repo, _changes ->
+      result =
+        Repo.insert_all(
+          "user_guilds",
+          [%{user_id: user_id, guild_id: guild_id, inserted_at: now, updated_at: now}],
+          on_conflict: :nothing,
+          conflict_target: [:user_id, :guild_id]
+        )
+
+      {:ok, result}
+    end)
+    |> Repo.transaction()
+    |> case do
+      {:ok, %{user: user}} -> {:ok, user}
+      {:error, :user, changeset, _} -> {:error, changeset}
+      {:error, :user_guild, reason, _} -> {:error, reason}
+    end
   end
 
   @doc """
   Same as update_last_interaction/1 but raises on error.
   """
-  @spec update_last_interaction!(integer()) :: User
-  def update_last_interaction!(user_id) do
-    case update_last_interaction(user_id) do
+  @spec update_last_interaction!(Snowflake.t(), Snowflake.t()) :: User
+  def update_last_interaction!(guild_id, user_id) do
+    case update_last_interaction(guild_id, user_id) do
       {:ok, user} -> user
       {:error, changeset} -> raise Ecto.InvalidChangesetError, changeset: changeset
     end
